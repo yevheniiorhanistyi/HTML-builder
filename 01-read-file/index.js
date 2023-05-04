34c2f9203592191e876df134c440df98b3c7dfb7
Yevhenii <organistiy@gmail.com>
2023-05-04 11:15:57 +0200

    feat: add solution to 01 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,7 @@
-const fs = require('fs');
 const path = require('path');
+const { createReadStream } = require('fs');
+
+const readStream = createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+readStream.on('data', data => console.log(data))
+readStream.on('error', error => console.log(error.message))
