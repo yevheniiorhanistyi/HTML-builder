927b60624490d6ad862435231c334e68a774d412
Yevhenii <organistiy@gmail.com>
2023-05-05 12:10:37 +0200

    feat: add solution 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { stdin, stdout } = process;
+const { createWriteStream } = require('fs');
+const { createInterface } = require('readline');
+
+const rl = createInterface({
+    input: stdin,
+    output: stdout
+});
+
+const writeStream = createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+console.log('Welcome! Please enter some text:');
+
+rl.on('line', (input) => {
+    if (input.toLowerCase() === 'exit' || input === '\u0003') {
+        console.log('Goodbye!');
+        process.exit();
+    } else {
+        writeStream.write(`${input}\n`);
+    }
+});
